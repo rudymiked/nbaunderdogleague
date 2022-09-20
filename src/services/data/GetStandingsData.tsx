382b48bd7e4b53645c5d73855c8d8a3a2f4dd6b0
rudymiked <rudymiked@gmail.com>
2022-09-20 14:55:35 -0700

    standings https

--- src/services/data/GetStandingsData.tsx
+++ src/services/data/GetStandingsData.tsx
@@ -4,7 +4,7 @@ const GetStandingsData = () => {
     const httpService = HttpService();
 
     return httpService.get({
-        url: "http://data.nba.net/prod/v1/current/standings_all.json",
+        url: "https://data.nba.net/prod/v1/current/standings_all.json",
         token: null,
         params: {}
     });
