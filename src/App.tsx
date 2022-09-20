68252af78ba8de9236779f63dd593ac4db40b180
rudymiked <rudymiked@gmail.com>
2022-09-20 14:07:04 -0700

    converting app to typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <p>NBA Underdogs</p>
+    </div>
+  );
+}
+
+export default App;
