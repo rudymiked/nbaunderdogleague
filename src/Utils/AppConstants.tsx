45102f82e3ad40b946dcb4ef8b032a447bbe9682
rudymiked <rudymiked@gmail.com>
2023-01-02 15:03:27 -0800

    Update AppConstants.tsx
    updated year -- need to fix this fully after break.

--- src/Utils/AppConstants.tsx
+++ src/Utils/AppConstants.tsx
@@ -1,4 +1,4 @@
-export const CURRENT_YEAR = (new Date()).getFullYear();
+export const CURRENT_YEAR = (new Date()).getFullYear() - 1;
 export const NBAStartDate: Date = new Date("10/18/" + CURRENT_YEAR);
 export const NBAEndDate: Date = new Date("7/01/" + CURRENT_YEAR);
 
