e92de6eede6f17741d34dd367661cf2eff8f03ce
rudymiked <rudymiked@gmail.com>
2023-01-08 14:26:42 -0800

    updated CURRENT_YEAR calculation in uI

--- src/Utils/AppConstants.tsx
+++ src/Utils/AppConstants.tsx
@@ -1,4 +1,7 @@
-export const CURRENT_YEAR = (new Date()).getFullYear() - 1;
+export const CURRENT_DATE: Date = new Date();
+
+// switch seasons in July, after the playoffs are over.
+export const CURRENT_YEAR: number = CURRENT_DATE.getMonth() < 7 ? CURRENT_DATE.getFullYear() - 1 : CURRENT_DATE.getFullYear();
 export const NBAStartDate: Date = new Date("10/18/" + CURRENT_YEAR);
 export const NBAEndDate: Date = new Date("7/01/" + CURRENT_YEAR);
 
